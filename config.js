7ca94faad960c46f315f3270ec39394d450228d1
g.mellone <mellone.gennaro@hotmail.com>
2020-11-27 17:41:34 +0100

    files details and config

---
+++ config.js
@@ -0,0 +1,20 @@
+let config = {};
+
+config.folders = [{
+    name:'solutions',
+    yapexil:'embeddables/',
+    mef: ''
+    },{
+    name:'statements',
+    yapexil:'statements/',
+    mef: ''
+},{
+    name:'tests',
+    yapexil:'tests/',
+    mef: ''
+}
+];
+
+
+
+module.exports = config;
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const unzipper = require("unzipper");
 const fs = require("fs");
+let stream = require('stream');
+const readline = require('readline');
+const etl = require("etl");
+
+let config = require('./config');
 
 exports.yapexil2mef = function(path) {
     let file = fs.createReadStream(path);
@@ -7,14 +12,49 @@ exports.yapexil2mef = function(path) {
 }
 
 exports.yapexil2mefStream = async function (file) {
-    const zip = file.pipe(unzipper.Parse({forceStream: true}));
+    file
+        .pipe(unzipper.Parse())
+        .pipe(etl.map(async entry => {
+            //SEARCH FOLDERS
+            let folders = config.folders;
 
-    for await (const entry of zip) {
-        const fileName = entry.path;
-        const type = entry.type; // 'Directory' or 'File'
-    //Test comment
-        if (fileName === "metadata.json"){
-            console.log(entry);
-        }
+            for (const x in folders){
+                let folder = folders[x];
+                let file = getFilesInfo(entry.path);
+
+                if ((file.fullPath).match(folder.yapexil)){ //If FILE is in some FOLDERS
+                    console.log(file);
+                }
+            }
+            /*
+            if (entry.path === "metadata.json") {
+                const content = await entry.buffer();
+                let str = content.toString();
+                console.log(JSON.parse(str));
+
+
+
+                //await fs.writeFile('output/path',content); //Usare alla fine per scrivere il file MEF
+            }
+            else {
+                entry.autodrain();
+            }
+
+             */
+        }))
+}
+
+getFilesInfo = function (fileName){
+    let extension = fileName.split(".")[1];
+
+    let temp_name = fileName.split("/");
+    let name = temp_name[temp_name.length-1];
+
+    return {
+        name: name,
+        extension: extension,
+        folder:temp_name[0],
+        fullPath: fileName
     }
+
 }
